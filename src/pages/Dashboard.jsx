e4ee3aff682bc4ac18f79a2101367c66f204d621
sharonkorir <csharonkorir@gmail.com>
2023-09-11 12:25:48 +0300

    Part2: components and style props

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,5 +1,33 @@
+import { Container, Heading, Text, Box } from "@chakra-ui/react";
+
 export default function Dashboard() {
+  const boxStyles = {
+    p: "10px",
+    bg: "purple.400",
+    color: "white",
+    m: "10px",
+    textAlign: "center",
+    filter: "blur(2px)",
+    ":hover": {
+      color: "black",
+      bg: "blue.400",
+    },
+  };
   return (
-    <div>Dashboard</div>
-  )
+    <Container as="section" maxW={"4xl"} py={"20px"}>
+      <Heading my={"30px"} p={"10px"}>
+        Dashboard
+      </Heading>
+      <Text marginLeft={"30px"}>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rerum, hic.
+      </Text>
+      <Text ml={"30px"} color={"blue"} fontWeight={"bold"}>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rerum, hic.
+      </Text>
+      <Box my={"30px"} p={"20px"} bg={"orange"}>
+        <Text color={"white"}>This is a box</Text>
+      </Box>
+      <Box sx={boxStyles}>Hola!</Box>
+    </Container>
+  );
 }
