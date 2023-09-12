2be3da5da90bad0479fe7c11c0570883d3a46a57
sharonkorir <csharonkorir@gmail.com>
2023-09-12 12:53:47 +0300

    part 4: grid layouts

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,33 +1,22 @@
-import { Container, Heading, Text, Box } from "@chakra-ui/react";
+import { SimpleGrid, Box } from "@chakra-ui/react";
 
 export default function Dashboard() {
-  const boxStyles = {
-    p: "10px",
-    bg: "purple.400",
-    color: "white",
-    m: "10px",
-    textAlign: "center",
-    filter: "blur(2px)",
-    ":hover": {
-      color: "black",
-      bg: "blue.400",
-    },
-  };
   return (
-    <Container as="section" maxW={"4xl"} py={"20px"}>
-      <Heading my={"30px"} p={"10px"}>
-        Dashboard
-      </Heading>
-      <Text marginLeft={"30px"}>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rerum, hic.
-      </Text>
-      <Text ml={"30px"} color={"blue"} fontWeight={"bold"}>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rerum, hic.
-      </Text>
-      <Box my={"30px"} p={"20px"} bg={"orange"}>
-        <Text color={"white"}>This is a box</Text>
-      </Box>
-      <Box sx={boxStyles}>Hola!</Box>
-    </Container>
+    <SimpleGrid p={"10px"} columns={4} spacing={10} minChildWidth={"250px"}>
+      <Box bg={"white"} h={"200px"} border={"1px solid"}></Box>
+      <Box bg={"white"} h={"200px"} border={"1px solid"}></Box>
+      <Box bg={"white"} h={"200px"} border={"1px solid"}></Box>
+      <Box bg={"white"} h={"200px"} border={"1px solid"}></Box>
+
+      <Box bg={"white"} h={"200px"} border={"1px solid"}></Box>
+      <Box bg={"white"} h={"200px"} border={"1px solid"}></Box>
+      <Box bg={"white"} h={"200px"} border={"1px solid"}></Box>
+      <Box bg={"white"} h={"200px"} border={"1px solid"}></Box>
+
+      <Box bg={"white"} h={"200px"} border={"1px solid"}></Box>
+      <Box bg={"white"} h={"200px"} border={"1px solid"}></Box>
+      <Box bg={"white"} h={"200px"} border={"1px solid"}></Box>
+      <Box bg={"white"} h={"200px"} border={"1px solid"}></Box>
+    </SimpleGrid>
   );
 }
